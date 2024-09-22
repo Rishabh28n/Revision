dcdb7416183f242980762c9ee4fd461286c0161f
Rishabh28n <rishabh.07n@gmail.com>
2024-09-22 16:56:26 +0530

    Add new javascript file

---
+++ app.js
@@ -0,0 +1,16 @@
+//Add javascript code here
+function add(num1, num2) {
+        return num1 + num2;
+    }
+    function subtract(num1, num2) {
+        return num1 - num2;
+    }
+    function multiply(num1, num2) {
+        return num1 * num2;
+    }
+    function divide(num1, num2) {
+        return num1 / num2;
+    }
+     console.log(add(1, 2));
+     console.log(subtract(1, 2));
+     console.log(multiply(1, 2));   
